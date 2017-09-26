2a6b2fd6b52ddec374d37210178748a8734c01e1
ramvs1994 <31643875+ramvs1994@users.noreply.github.com>
2017-09-26 12:20:16 +0530

    Update insert.c

--- avl/src/insert.c
+++ avl/src/insert.c
@@ -1,5 +1,5 @@
 #include"header.h"
-
+/* this function is to insert a node into the tree structure and check for balacing after insertion by traversing back */
 struct node *insert(struct node *root, int value)
 {
 	if(root == NULL) {
@@ -8,8 +8,8 @@ struct node *insert(struct node *root, int value)
 		root->left = NULL ;
 		root->right = NULL ;
 		return root ;
-
-	} else if(root->data >= value) {
+/* traverse recurrsivly till suitable place as per BST norms is found */
+	} else if(root->data >= value) { 
 		
 		root->left = insert(root->left, value) ;	
 
@@ -22,24 +22,24 @@ struct node *insert(struct node *root, int value)
 
 	ht_lt = height(root->left) ;
 	ht_rt = height(root->right) ;
-	balance_factor = ht_lt - ht_rt ;
+	balance_factor = ht_lt - ht_rt ; /* calculating the balance factor for current node */
 
-	if((balance_factor > 1) && (value < root->left->data))
+	if((balance_factor > 1) && (value < root->left->data))  /* left left imbalance condition*/
 		
-		return right_rot(root) ;
+		return right_rot(root) ;			/* correcting left left imbalance by right rotation*/
 
-	if((balance_factor < -1) && (value > root->right->data))
-		return left_rot(root) ;
+	if((balance_factor < -1) && (value > root->right->data)) /* right rigth imbalance condition */
+		return left_rot(root) ;			/* correcting right rigth imbalance by left rotation */
 
-	if((balance_factor > 1) && (value > root->left->data)) {
+	if((balance_factor > 1) && (value > root->left->data)) { /* left right imbalance condition */
 
-		root->left = left_rot(root->left) ;
+		root->left = left_rot(root->left) ; /* correcting left right imbalance by left rotation and then right rotation */
 		return right_rot(root) ;
 	}
 
-	if((balance_factor < -1 && value < root->right->data)) {
+	if((balance_factor < -1 && value < root->right->data)) { /* right left imbalance condition */
 		
-		root->right = right_rot(root->right) ;
+		root->right = right_rot(root->right) ; /*correction for right left imbalance by right rotation and then left rotation */
 		return left_rot(root) ;
 	}
 
