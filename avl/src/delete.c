29d3054d682f4a1822268ff46b763793df589845
ramvs1994 <31643875+ramvs1994@users.noreply.github.com>
2017-09-26 12:12:02 +0530

    Update delete.c

--- avl/src/delete.c
+++ avl/src/delete.c
@@ -1,5 +1,5 @@
 #include"header.h"
-
+/* this function is to delete a node from the tree and balancing after deletion */ 
 struct node* delete(struct node *root, int value)
 {
 	struct node *min ;
@@ -37,21 +37,21 @@ struct node* delete(struct node *root, int value)
 	int balance_fact,ht_lst,ht_rst ;
 //	ht_lst = height(root->left) ;
 //	ht_lst = height(root->right) ;
-	balance_fact = balance_factor(root) ;
+	balance_fact = balance_factor(root) ;   /* calculating balance factor of current node */
 	
-	if((balance_fact > 1) && (balance_factor(root->left) >=0))
-		return right_rot(root) ;
+	if((balance_fact > 1) && (balance_factor(root->left) >=0))   /* condition for left left imbalance */
+		return right_rot(root) ;		/*correcting left left error by right rotating */	
 
-	if((balance_fact > 1) && (balance_factor(root->left) == -1)) {
-		root->left = left_rot(root->left) ;		
+	if((balance_fact > 1) && (balance_factor(root->left) == -1)) {   /* condition for left right imbalance  */
+		root->left = left_rot(root->left) ;	/* correcting left right error by left rotation and the right rotation*/
 		return right_rot(root) ;
 	}
 	
-	if((balance_fact < -1) && (balance_factor(root->right) <= 0)) 
-		return left_rot(root) ;
+	if((balance_fact < -1) && (balance_factor(root->right) <= 0)) /*condition for right right imbalance */
+		return left_rot(root) ;			/* correcting right right error by left rotation */
 
-	if(balance_fact < -1 &&(balance_factor(root->right)> 0)) {
-		root->right = right_rot(root->right) ;
+	if(balance_fact < -1 &&(balance_factor(root->right)> 0)) { /*condition for right left imbalance */
+		root->right = right_rot(root->right) ; /* correcting right left error right rotation followed by left rotation */
 		return left_rot(root) ;
 	}
 
